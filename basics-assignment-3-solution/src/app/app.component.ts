35f79ed675f0849ec224d74ab1922ed11e18d4e0
Andrew Bateman <gomezbateman@yahoo.com>
2019-02-04 08:22:36 +0100

    assignment 3 completed

---
+++ basics-assignment-3-solution/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  showSecret = false;
+  log = [];
+
+  toggleText() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
+
+}
+
+
