d8f690ae622ecf0a9bf20f77995a4dd0a147f406
Andrew Bateman <gomezbateman@yahoo.com>
2019-02-16 12:18:39 +0100

    assignment7 reactive forms completed

--- forms-reactive-assignment-7-solution/src/app/app.component.ts
+++ forms-reactive-assignment-7-solution/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  dataForm: FormGroup;
+  listStatus = [
+    'Stable',
+    'Critical',
+    'Finished'
+  ]
+  forbiddenProjectNames = ['Test'];
+
+  ngOnInit() {
+    this.dataForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required], this.forbiddenNamesPromise.bind(this)),
+      'email': new FormControl(
+        null,
+        [
+          Validators.required,
+          Validators.email,
+        ],
+      ),
+      'status': new FormControl('Stable'),
+    });
+  }
+
+  // Validator
+  forbiddenNames(control: FormControl): {[s: string]: boolean} { // Key: value
+    if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true}
+    }
+    return null;
+  }
+
+  forbiddenNamesPromise(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+          resolve({'nameIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  onSubmit() {
+    console.log(this.dataForm);
+    console.log(this.dataForm.value.projectName)
+  }
+
 }
